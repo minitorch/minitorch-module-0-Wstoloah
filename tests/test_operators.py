c356cf6a200ca27c43dca657af16ae7ddfaec6b9
Ouissal <ouissal.boutouatou@student-cs.fr>
2025-06-25 21:41:18 +0200

    simple operators test functions

--- tests/test_operators.py
+++ tests/test_operators.py
@@ -20,6 +20,7 @@ from minitorch.operators import (
     neg,
     negList,
     prod,
+    exp,
     relu,
     relu_back,
     sigmoid,
@@ -108,7 +109,10 @@ def test_sigmoid(a: float) -> None:
     * It is  strictly increasing.
     """
     # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert  0 < sigmoid(a) < 1
+    assert_close(1 - sigmoid(a),sigmoid(-a))
+    assert sigmoid(0) == 0.5
+    assert sigmoid(a + 0.1) > sigmoid(a)
 
 
 @pytest.mark.task0_2
@@ -116,32 +120,36 @@ def test_sigmoid(a: float) -> None:
 def test_transitive(a: float, b: float, c: float) -> None:
     """Test the transitive property of less-than (a < b and b < c implies a < c)"""
     # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    if a < b and b < c:
+        assert a < c
 
 
 @pytest.mark.task0_2
-def test_symmetric() -> None:
+@given(small_floats, small_floats)
+def test_symmetric(a: float, b:float) -> None:
     """Write a test that ensures that :func:`minitorch.operators.mul` is symmetric, i.e.
     gives the same value regardless of the order of its input.
     """
     # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert_close(mul(a, b),mul(b, a))
 
 
 @pytest.mark.task0_2
-def test_distribute() -> None:
+@given(small_floats, small_floats, small_floats)
+def test_distribute(a: float, b: float, c: float) -> None:
     r"""Write a test that ensures that your operators distribute, i.e.
     :math:`z \times (x + y) = z \times x + z \times y`
     """
     # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert_close(mul(c, a+b),mul(c,a) + mul(c,b))
 
 
 @pytest.mark.task0_2
-def test_other() -> None:
+@given(small_floats)
+def test_other(a: float) -> None:
     """Write a test that ensures some other property holds for your functions."""
     # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert exp(a) > 0
 
 
 # ## Task 0.3  - Higher-order functions
